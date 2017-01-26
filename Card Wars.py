6017960c23cf7db8d08c9aeb56bff2e4811be91a
BaMItZDenZ <bamitzdenz@outlook.com>
2017-01-26 11:42:39 +0000

    Add files via upload

---
+++ Card Wars.py
@@ -0,0 +1,30 @@
+import random
+
+name = input("Hello, what is your username going to be?:")
+
+decksize = input("Hello and welcome to card wars what kind of deck size are you going to play with 3,6,10 or 13?:")
+print("Ok so you are going to play with a deck size of", (decksize),"cool.")
+player = input("Would you like to play with another person or with the computer?(person,computer):")
+if 
+
+
+
+elif player == 'person':
+    nametwo = input("Ok so you are going to play with another person. What will his/her username be?:")
+    print("Welcome", (nametwo))
+    
+    
+    
+    
+    
+    
+    
+    
+
+elif player == 'computer':
+    print("Have fun with the computer.")
+    
+    
+
+
+
